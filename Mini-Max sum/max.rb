6f49e0c853081e62fce7fafd283812c63857dd5b
Andre Garcia <andrejulio072@hotmail.com>
2022-07-09 22:57:12 +0100

    challenge max sum

---
+++ Mini-Max sum/max.rb
@@ -0,0 +1,24 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr)
+    # Write your code here
+    arr.sort!
+    sum=arr.sum
+    max=sum-arr[0]
+    min=sum-arr[-1]
+    puts("#{min} #{max}")
+
+end
+
+arr = gets.rstrip.split.map(&:to_i)
+
+miniMaxSum arr
